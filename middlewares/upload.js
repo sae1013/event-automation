59b73999d27e65b72dc46578969e385d8daac32a
sae1013 <sae1013@gmail.com>
2022-10-29 03:37:44 +0900

    에러핸들링 수정 & 이벤트 수정하기 기능 추가 - multer field 업로드 추가

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,10 +1,10 @@
 const uploadImageMiddleware = async (req, res,next) => {
   const uploadedImages = req.files;
-  const imagePaths = uploadedImages.map((img) => img.location);
-  if (!uploadedImages) {
-    res.status(400).json({ message: "이미지가 없습니다!" });
-  }
-  // res.status(200).json(imagePaths);
+  const eventImageUrls = uploadedImages?.eventImageUrl;
+  const eventKeyVisualImageUrls = uploadedImages?.eventKeyVisualImageUrl;
+
+  eventImageUrls ? req.eventImageUrl = eventImageUrls[0].location : null
+  eventKeyVisualImageUrls ? req.eventKeyVisualImageUrl = eventKeyVisualImageUrls[0].location : null
   next();
 }
 module.exports = {
--- routes/event.js
+++ routes/event.js
@@ -38,15 +38,23 @@ eventRouter.get("/:eventId",asyncHandler(async(req,res) => {
 }))
 
 // 이벤트 등록
-eventRouter.post("/enroll",upload.array("eventImageUrl", 4),uploadImageMiddleware, async(req,res) => {
-  const eventImageUrl = req.files[0].location;
-  const eventKeyVisualImageUrl = req.files[1]?.location;
+eventRouter.post("/enroll",upload.fields([{name:'eventImageUrl'},{name:'eventKeyVisualImageUrl'}]),uploadImageMiddleware, asyncHandler(async(req,res) => {
+  const eventImageUrl = req.eventImageUrl
+  const eventKeyVisualImageUrl = req.eventKeyVisualImageUrl;
 
   let {eventId,eventTitle,eventStartDate,eventEndDate,eventFooter,eventArea} = req.body;
   eventFooter = JSON.parse(eventFooter)
   eventArea = JSON.parse(eventArea);
-  
+  let errorMessage = "이벤트 생성에 실패했습니다."
   try{
+    const existEvent = await EventModel.findOne({
+      eventId
+    });
+    if(existEvent) {
+      errorMessage = '해당 고유번호는 이미 등록된 이벤트 입니다'
+      throw Error()
+    }
+
     const generatedEvent = await EventModel.create({
       eventId,
       eventTitle,
@@ -59,10 +67,42 @@ eventRouter.post("/enroll",upload.array("eventImageUrl", 4),uploadImageMiddlewar
     })
     res.json(generatedEvent)
   }catch(err){
-    throw Error('등록이 실패했습니다')
+    throw Error(errorMessage)
   }
   
-})
+}))
+
+eventRouter.post("/edit",upload.fields([{name:'eventImageUrl'},{name:'eventKeyVisualImageUrl'}]),uploadImageMiddleware, asyncHandler(async(req,res) => {
+
+  console.log(req.file)
+  let {eventId,eventTitle,eventStartDate,eventEndDate,eventFooter,eventArea} = req.body;
+  eventFooter = JSON.parse(eventFooter)
+  eventArea = JSON.parse(eventArea);
+
+  const eventImageUrl = req.eventImageUrl;
+  const eventKeyVisualImageUrl = req.eventKeyVisualImageUrl;
+
+  const updateState = {
+    eventTitle,
+    eventStartDate,
+    eventEndDate,
+    eventFooter,
+    eventArea,
+  }
+
+  eventImageUrl ? updateState.eventImageUrl = eventImageUrl : null
+  eventKeyVisualImageUrl ? updateState.eventKeyVisualImageUrl = eventKeyVisualImageUrl:null
+
+  try{
+    await EventModel.findOneAndUpdate({eventId:eventId},updateState,{ new:true })
+    res.json({message:'수정이 완료되었습니다'})
+
+  }catch(err){
+    console.log('에러',err)
+    throw Error('편집이 실패했습니다')
+  }
+
+}))
 
 eventRouter.delete("/", asyncHandler(async(req,res) => {
   const {eventId} = req.body;
