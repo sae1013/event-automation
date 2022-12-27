f6bf3088d4e2382c8f4b14197d69c50f86463996
sae1013 <sae1013@gmail.com>
2022-12-28 02:14:15 +0900

    FIX: Change Deploy Server from Heroku to Fly.io

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const env = import.meta.env.MODE
 
-const baseUrl = env === 'development' ? 'http://127.0.0.1:8080' : 'https://event-auto-generator.herokuapp.com'
+const baseUrl = env === 'development' ? 'http://127.0.0.1:8080' : 'https://event-auto.fly.dev'
 
 const axiosInstance = axios.create({
   baseURL: baseUrl,
