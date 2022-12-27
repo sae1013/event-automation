e312db2479c95eb2aecfffdb3d90b2937f968147
sae1013 <sae1013@gmail.com>
2022-12-28 03:19:58 +0900

    FIX: Change Backend Domain Name

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const env = import.meta.env.MODE
 
-const baseUrl = env === 'development' ? 'http://127.0.0.1:8080' : 'https://event-auto.fly.dev'
+const baseUrl = env === 'development' ? 'http://127.0.0.1:8080' : 'https://event-maker.fly.dev'
 
 const axiosInstance = axios.create({
   baseURL: baseUrl,
